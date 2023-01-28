8dcf28020f77678213e02a9c18964a8614b40a73
keshan_mwm <mwmadushakeshan99@gmail.com>
2023-01-28 11:11:58 +0530

    Update script.js

--- Assignments/module_4/script.js
+++ Assignments/module_4/script.js
@@ -30,7 +30,7 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-var names = ["keshan", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
